378e7be79afcfa5c706a163628b9224b4e8c3cde
kazuki3 <45926663+kazuki3@users.noreply.github.com>
2022-05-02 16:03:42 +0900

    page作成

--- src/App.js
+++ src/App.js
@@ -1,28 +1,6 @@
-import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
-import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
-import { SearchInput } from "./components/molecules/SearchInput";
-import { UserCard } from "./components/organisms/user/UserCard";
+import { Router } from "./router/Router";
 import "./styles.css";
 
-const user = {
-  name: "Jessy",
-  image: "https://source.unsplash.com/_0PtmUlNqiY",
-  email: "1234@example.com",
-  tel: "090-1111-2222",
-  company: {
-    name: "テスト株式会社"
-  },
-  website: "https://google.com"
-};
-
 export default function App() {
-  return (
-    <div className="App">
-      <PrimaryButton>テスト</PrimaryButton>
-      <SecondaryButton>テスト</SecondaryButton>
-      <br />
-      <SearchInput />
-      <UserCard user={user} />
-    </div>
-  );
+  return <Router />;
 }
---
+++ src/components/templates/HeaderOnly.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+import { Header } from "../atoms/layout/Header";
+export const HeaderOnly = (props) => {
+  const { children } = props;
+  return (
+    <>
+      <Header />
+      {children}
+    </>
+  );
+};
